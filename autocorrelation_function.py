c165f19c9887002442592090944a191992734af3
Pedro Sartori <pedrosartdr@gmail.com>
2025-04-10 22:48:45 -0300

    Realiza o cálculo do prêmio de risco (Word)

--- autocorrelation_function.py
+++ autocorrelation_function.py
@@ -1,23 +1,30 @@
 import yfinance as yf
 import numpy as np
 import seaborn as sns
+import plotnine as gg
 
 # Lendo os dados da bolsa de 2018 até hoje
-df = yf.download(["^BVSP"], start="2018-01-01", end="2025-01-01")
+start_date = ["2007", "01", "01"]
+end_date = ["2010", "04", "08"]
+df = yf.download(
+    ["^BVSP"], 
+    start="-".join(start_date), 
+    end="-".join(end_date)
+)
 
 # Removendo as colunas duplas e removendo as datas dos índices
 df.columns = [col[0] for col in df.columns]
 df = df.reset_index()
 
-# Colocando o preço de fechamento na variável s
+# Selecionando o preço de fechamento
 s = df["Close"]
 
 # Calculando o log-price-return
 r = np.log(s / s.shift(1))
 r = r.dropna()
 
-# Calculando a volatilidade
-v = np.abs(r)
+# Calculando a volatilidade instantânea
+v = np.sqrt(np.square(r) - np.square(np.mean(r)))
 
 # Calculando a função de autocorrelação para r e v
 Rr = [((r.shift(i) * r).mean() - r.mean()**2) / r.std()**2 for i in range(100)]
@@ -27,5 +34,23 @@ Rv = [((v.shift(i) * v).mean() - v.mean()**2) / v.std()**2 for i in range(100)]
 sns.lineplot(y=s, x=range(len(s)))
 sns.lineplot(y=r, x=range(len(r)))
 sns.lineplot(y=v, x=range(len(v)))
-sns.lineplot(y=Rr, x=range(100))
+sns.lineplot(y=np.log(Rr[1:]), x=np.log(list(range(100))[1:]))
 sns.lineplot(y=np.log(Rv), x=np.log(range(100)))
+
+start_date_formatado = "/".join(list(reversed(start_date)))
+end_date_formatado = "/".join(list(reversed(end_date)))
+gg.ggplot() + \
+    gg.geom_line(gg.aes(x=df.Date, y=s)) + \
+    gg.scale_x_datetime(date_labels="%d/%m/%Y") + \
+    gg.theme_light() + \
+    gg.labs(
+        y="Fechaamento"
+    ) + \
+    gg.ggtitle(
+        f"Fechamento do IBovespa de {start_date_formatado} a {end_date_formatado}"
+    ) + \
+    gg.theme(
+     panel_border=gg.element_blank(),
+     axis_title_x=gg.element_blank(),
+     axis_text_x=gg.element_text(angle=45)
+    )
