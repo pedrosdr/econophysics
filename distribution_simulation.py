46efd54c74d43e4a6fa784740daddcea2c8e19bf
Pedro Sartori <pedrosartdr@gmail.com>
2025-05-05 22:23:15 -0300

    refactor: Cria uma função para criar o gráfico das distribuições em ggplot

--- distribution_simulation.py
+++ distribution_simulation.py
@@ -2,43 +2,50 @@ import numpy as np
 import pandas as pd
 import scipy as sp
 import seaborn as sns
+import plotnine as gg
 
 
-def levy(n:int, precision:int=999999) -> np.ndarray:
-    """
-    Gera uma amostra aleatória a partir da distribuição de Levy
-    """
-    probs = sp.stats.levy.pdf(np.linspace(0.0, 1000.0, precision))
-    probs = probs / probs.sum()
-    return np.random.choice(
-        np.linspace(0.0, 1000.0, precision),
-        n,
-        p=probs
-
-    )
+def distplot(x, min_y=None, max_y=None, title=None):
+    plot = gg.ggplot() + gg.theme_light() +\
+        gg.geom_line(
+            mapping = gg.aes(
+                x=np.arange(len(x)),
+                y=x
+            )    
+        ) +\
+        gg.labs(
+            y="Variável X",
+            x="Tempo"
+        ) +\
+        gg.theme(
+            panel_grid=gg.element_blank(),
+            plot_title=gg.element_text(hjust=0.5)
+        )
+    
+    if min_y is not None and max_y is not None:
+        plot = plot +\
+            gg.coord_cartesian(ylim=[min_y, max_y])
+            
+    if title is not None:
+        plot = plot +\
+            gg.ggtitle(title)
+            
+    return plot
 
 
 # Define o tamanho da amostra
-size = 300
+size = 1000
 
 # Levy
-x = levy(size)
-sns.histplot(x=x, bins=int(size/2))
-sns.lineplot(x=range(size), y=x)
-
-# Levy com inversão de sinal aleatória
-x = levy(size)
-pos = np.random.choice([True, False], size)
-x = np.where(pos, x, -x)
-sns.histplot(x=x, bins=int(size/2))
-sns.lineplot(x=range(size), y=x)
+x = sp.stats.levy.rvs(size = size)
+distplot(x, title="Distribuição Levy Padrão")
+distplot(x, min_y=0, max_y=30, title="Distribuição Levy Padrão (Com zoom)")
 
 # Normal
 x = np.random.randn(size)
-sns.histplot(x=x, bins=int(size/2))
-sns.lineplot(x=range(size), y=x)
+distplot(x, title="Distribuição Normal Padrão")
 
 # Cauchy
-x = np.random.standard_cauchy(size)
-sns.histplot(x=x, bins=int(size/2))
-sns.lineplot(x=range(size), y=x)
+x = sp.stats.cauchy.rvs(size=size)
+distplot(x, title="Distribuição Cauchy (Com zoom)")
+distplot(x, min_y=-10, max_y=10, title="Distribuição Cauchy (Com zoom)")
